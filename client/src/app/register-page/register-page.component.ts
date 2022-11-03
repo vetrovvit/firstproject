d75d3e7d1ac1861f627774f595b812761609cb3f
Vitalii Vetrov <103350845+vetrovvit@users.noreply.github.com>
2022-11-03 08:59:02 +0200

    change_01

--- client/src/app/register-page/register-page.component.ts
+++ client/src/app/register-page/register-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../shared/services/auth.service";
+import {error} from "@angular/compiler-cli/src/transformers/util";
+import {Router} from "@angular/router";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-register-page',
@@ -7,9 +12,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPageComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup
+  aSub: Subscription
+
+  constructor(private auth: AuthService,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)])
+    })
+  }
+
+  onSubmit(){
+    this.form.disable()
+    this.auth.register(this.form.value).subscribe(
+      () => {
+        this.router.navigate(['/login'], {
+          queryParams: {
+            registered: true
+          }
+        })
+      },
+      error => {
+        console.warn(error)
+        this.form.enable()
+      }
+    )
   }
 
 }
